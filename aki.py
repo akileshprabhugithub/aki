8cff07f5eb23c4d64f1d9592957aef30dfc8b62f
eyepatchakilesh69 <31615067+eyepatchakilesh69@users.noreply.github.com>
2017-09-04 14:29:18 +0530

    Add files via upload

---
+++ aki.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import random
+count=0
+r=0
+while count<=100:
+	roll=input("press r to roll the dice :")
+	if roll=="r":
+		r=random.randint(1,6)
+		count=count+r
+		print("your random num is",r)
+		print("you are on count",count)
+
+		if count>=100:
+			print("well done")
+		elif count==2:
+			print("climb ladder to 34")
+			count=34
+		elif count==8:
+			print("climb ladder to 37")
+			count=37
+		elif count==40:
+			print("climb ladder to 68")
+			count=68
+		elif count==52:
+			print("climb ladder to 81")
+			count=81
+		elif count==76:
+			print("climb ladder to 96")
+			count=96
+		
+
+	
