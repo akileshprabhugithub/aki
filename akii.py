67eef9b984b247464f589caf83b0ae687c8d4487
eyepatchakilesh69 <31615067+eyepatchakilesh69@users.noreply.github.com>
2017-09-04 14:33:15 +0530

    Add files via upload

--- akii.py
+++ akii.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import random 
-i=0
-while(i<100):
-	n=input("press r ro roll the dice")
-	if(n=='r'):
-		r=myroll()
-		i=i+r
-		print("new podtion is",i )
+for i in range(0,20):
+	if(i%2==0):
+		print(i)
+
+
+
+	
